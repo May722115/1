2824d1f0c9069ea75e0f9044e887b68f7ae328a9
Su Nan <sunanbjergsen@163.com>
2024-11-10 16:57:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,19 +12,21 @@ int main()
         cout<<"Type \"records\" to show the records of the games"<<endl;
         string input;
         cin>>input;
-        if(input == "new"){
+        if(input == "play"){
             
             play_game() 
             /*1.This function is a void function. 
               
-              2.It first ask user to input a difficulty level.Then a new game is randomly generated accordingly(maybe use 2D array)
-              (use an extra function for this step). The answer should be save for future comparision.
+              2.It first ask user to choose start a new game to load previous one.
+              
+              3.If user choose to start a new one ask user to input a difficulty level.Then a new game is randomly generated accordingly(maybe use 2D array)
+              (use an extra function for this step). The answer should be save as soon as a new game is generated or old one is loaded for future comparision.
               
               3.A timer should be set up to record.
              
-              4.Then the user input "add", "delete", "quit" or "save".
+              4.Then the user input "add", "remove", "quit" or "save".
                 
-                add,delete: add or remove a number in a specific place by editing the array.
+                add,remove: add or remove a number in a specific place by editing the array.
                 
                 quit: back to the main menu. Stop the timer. Add a record to the file.
                 
@@ -58,9 +60,10 @@ void play_game(){
     string input2;
     cin>>input2;      
         if(input2 == "new"){
-            
-            generate_game()
-            /* This function generate a new game for user.*/
+            string level;
+            cin>>level;
+            generate_game(level)
+            /* This function generate a new game for user depending on level chosed.*/
 
         }
         else if(input2 == "load"){
@@ -69,7 +72,7 @@ void play_game(){
             /* This function take the file saved before for user to play*/
         }
 
-        
+    //following part starts when a game is prepared and user begins to play.    
 
     string input3;
     cin>>input3;
@@ -78,7 +81,7 @@ void play_game(){
 
             add(row,column,number)//user fill in a number
 
-            check()//check whether the game is complete. If yes, quit and stop the timer. Add the time record to the file.
+            check()//check whether the game is complete. If yes, quit and stop the timer. Add the time record to the file(use another function).
             
         }
         else if(input3 == "remove") {
