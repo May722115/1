0ed9e9a839e11eaea59326100fd344ee96230f0a
Joanna-1234 <u3613638@connect.hku.hk>
2024-11-11 20:51:17 +0800

    Update generateFullboard.cpp

--- generateFullboard.cpp
+++ generateFullboard.cpp
@@ -3,26 +3,26 @@
 #include <string>
 using namespace std;
 
-//check whether generated number is valid (not repeated)
-bool checkvalid(int number, int row, int col){
-    //check for row
-    for (int i = 0; i < 9; i++){
-        if (numberboard[row][i] == number){
+//Check for validity of value generated at each position
+bool checkvalid(int numberboard[][9], int number, int row, int col) {
+    //check for columns
+    for (int i = 0; i < 9; i++) {
+        if (numberboard[row][i] == number) {
             return false;
         }
     }
 
-    //check for colunm
-    for (int i = 0; i < 9; i++){
-        if (numberboard[i][col] == number){
+    //check for rows
+    for (int i = 0; i < 9; i++) {
+        if (numberboard[i][col] == number) {
             return false;
         }
     }
 
     //check for 3x3 matrix
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            if (numberboard[(row / 3 * 3) + i][(col / 3 * 3) + j] == number){
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (numberboard[(row / 3 * 3) + i][(col / 3 * 3) + j] == number) {
                 return false;
             }
         }
@@ -30,36 +30,35 @@ bool checkvalid(int number, int row, int col){
     return true;
 }
 
-//Randomly generate a sudoku board filled with numbers
-bool generateboard(int numberboard[][9], int row, int col){
-    int number;
-    //Check whether all rows are filled
-    if (row == 8 && col == 9){
+//generate full number board
+bool generateboard(int numberboard[][9], int row, int col) {
+    //check whether all rows are filled
+    if (row == 8 && col == 9) {
         return true;
     }
-    //Check whether all columns are filled in this row
-    if (col == 9){
-        row += 1;
+
+    //check whether all colunms in the row are filled
+    if (col == 9) {
+        row++;
         col = 0;
     }
-    
-    //Check whether this place is filled
-    if (numberboard[row][col] != 0){
-        return generateboard(numberboard, row, col+1);
+
+    //check whether the position is filled
+    if (numberboard[row][col] != 0) {
+        return generateboard(numberboard, row, col + 1);
     }
 
-    //Generating for a whole row
-    for (int i = 1; i < 10; i++){
-        //Randomly choose a value
-        number = rand() % 9 + 1;
-        //check if the value fits the rule
-        if (checkvalid(number, row, col)){
+    //fill the number in a row
+    for (int i = 1; i <= 9; i++) {
+        //randomly generateing a number
+        int number = rand() % 9 + 1;
+        //check if the number if valid and fill it in the position
+        if (checkvalid(numberboard, number, row, col)) {
             numberboard[row][col] = number;
-            //Check whether next value can be chosen
-            if (generateboard(numberboard, row, col+1)){
+            //check whether next valid number exists, repeat previous step if not
+            if (generateboard(numberboard, row, col + 1)) {
                 return true;
             }
-            //Generate again if the chosen value cannot work
             numberboard[row][col] = 0;
         }
     }
