d6434dfad321335de8a22ddf5d6fb1fb4ae1663f
Alex0228haha <145734696+Alex0228haha@users.noreply.github.com>
2024-11-28 12:24:36 +0800

    Update play_game.cpp

--- play_game.cpp
+++ play_game.cpp
@@ -5,16 +5,3 @@
 #include <fstream>
 using namespace std;
 
-//add a number to the board
-void add(int row, int column, int number){
-  if (row< 0|| row>= size|| column>= size){
-    cout<< "The position is invalid!"<< endl;
-    return;
-  }
-  if (board[row][column] != 0){
-    cout<< "This position had alrewady filled!"<<endl;
-    return;
-  }
-  board[row][column] = number;
-  display_board();
-}
