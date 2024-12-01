ce9e39286991dfab10a2ac160241fd7ad871ba93
Joanna-1234 <u3613638@connect.hku.hk>
2024-12-01 14:56:48 +0800

    Update generate_hard.h

--- generate_hard.h
+++ generate_hard.h
@@ -2,5 +2,11 @@
 #define GENERATE_HARD_H
 
 bool checkvalid(int numberboard[][9], int number, int row, int col);
+bool generateboard(int numberboard[][9], int row, int col);
+bool checkremain(int numberboard[][9], int row, int col);
+void removehard(int numberboard[][9]);
+void formgameboard(vector<string> &board);
+void formatting(vector<string> &board, int numberboard[][9]);
+void storeanswer(int numberboard[][9]);
 
 #endif
