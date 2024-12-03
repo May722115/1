e87a08394119ad3ea15e0f8fb016a7f353d13f45
Joanna-1234 <u3613638@connect.hku.hk>
2024-12-03 15:06:59 +0800

    Update generate_hard.h

--- generate_hard.h
+++ generate_hard.h
@@ -5,8 +5,8 @@ bool checkvalid(int numberboard[][9], int number, int row, int col);
 bool generateboard(int numberboard[][9], int row, int col);
 bool checkremain(int numberboard[][9], int row, int col);
 void removehard(int numberboard[][9]);
-void formgameboard(vector<string> &board);
-void formatting(vector<string> &board, int numberboard[][9]);
+void formgameboard(std::vector<std::string> &board);
+void formatting(std::vector<std::string> &board, int numberboard[][9]);
 void storeanswer(int numberboard[][9]);
 
 #endif
