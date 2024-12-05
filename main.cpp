37b4b6d1ec287f834125efbefe89a66791f1b866
Su Nan <sunanbjergsen@163.com>
2024-12-05 13:10:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -154,7 +154,7 @@ void play_game(int sudoku[][9],int sudoku_copy[][9], vector<string> board){
     if(input2 == "new"){
             for(int i=0; i<9; i++){
                 for(int j=0; j<9; j++){
-                    sudoku[i][j]=0;
+                    sudoku[i][j]=0;}
             }
             const char* filename1 = "answer.txt";
             const char* filename2 = "save_game.txt";
@@ -179,9 +179,9 @@ void play_game(int sudoku[][9],int sudoku_copy[][9], vector<string> board){
             }
             else{
             removehard(sudoku);
+            }
             
-            
-            formatting(board, sudoku);}
+            formatting(board, sudoku);
 
         for (string &line : board) {
         cout << line << endl;
