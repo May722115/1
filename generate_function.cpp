070c8bc6dba9f33119b4c17f59dad548365ac8b0
Joanna-1234 <u3613638@connect.hku.hk>
2024-12-06 16:18:33 +0800

    Update generate_function.cpp

--- generate_function.cpp
+++ generate_function.cpp
@@ -13,8 +13,6 @@ int answer[9][9] = {0};
 int tempSudoku[9][9] = {0};
 int copy[9][9] = {0};
 
-bool checkvalid(int numberboard[][9], int number, int row, int col);
-bool generateboard(int numberboard[][9], int row, int col);
 bool hasSingleSolution(int sudoku[][9], int level);
 void removeElements(int sudoku[][9], int sudoku_copy[][9], int count, int level);
 void solve(int data[][9], int sudoku_copy[][9], int level);
@@ -45,72 +43,6 @@ int main() {
     }
 
     return 0;
-}
-
-bool checkvalid(int numberboard[][9], int number, int row, int col) {
-    // check for columns
-    for (int i = 0; i < 9; i++) {
-        if (numberboard[row][i] == number) {
-            return false;
-        }
-    }
-
-    // check for rows
-    for (int i = 0; i < 9; i++) {
-        if (numberboard[i][col] == number) {
-            return false;
-        }
-    }
-
-    // check for 3x3 matrix
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (numberboard[(row / 3 * 3) + i][(col / 3 * 3) + j] == number) {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
-// generate full number board
-bool generateboard(int numberboard[][9], int row, int col) {
-    // check whether all rows are filled
-    if (row == 8 && col == 9) {
-        return true;
-    }
-
-    // check whether all columns in the row are filled
-    if (col == 9) {
-        row++;
-        col = 0;
-    }
-
-    // check whether the position is filled
-    if (numberboard[row][col] != 0) {
-        return generateboard(numberboard, row, col + 1);
-    }
-
-    // fill the number in a row
-    for (int i = 1; i <= 9; i++) {
-        // randomly generating a number
-        int number = rand() % 9 + 1;
-
-        // check if the number is valid and fill it in the position
-        if (checkvalid(numberboard, number, row, col)) {
-            numberboard[row][col] = number;
-
-            // check whether next valid number exists, repeat previous step if not
-            if (generateboard(numberboard, row, col + 1)) {
-                return true;
-            }
-
-            numberboard[row][col] = 0;
-        }
-    }
-
-    return false;
 }
 
 bool hasSingleSolution(int sudoku[][9], int level) {
