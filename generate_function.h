25f5ac3456a74cad531c7f2880859a839914125c
Joanna-1234 <u3613638@connect.hku.hk>
2024-12-06 16:18:50 +0800

    Update generate_function.h

--- generate_function.h
+++ generate_function.h
@@ -15,8 +15,6 @@ extern int answer[9][9];
 extern int tempSudoku[9][9];
 extern int copy[9][9];
 
-bool checkvalid(int numberboard[][9], int number, int row, int col);
-bool generateboard(int numberboard[][9], int row, int col);
 bool hasSingleSolution(int sudoku[][9], int level);
 void removeElements(int sudoku[][9], int sudoku_copy[][9], int count, int level);
 void solve(int data[][9], int sudoku_copy[][9], int level);
